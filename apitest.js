9e2c2883df48325b31a9080ad619ddf3f44ec610
Daniel Molin <dmol@kth.se>
2014-02-21 17:16:33 +0100

    Loads country from musicbrainz

--- apitest.js
+++ apitest.js
@@ -3,11 +3,11 @@ var LASTFM_KEY = "865b1653dbe200905a5b75d9d839467a",
 
 var method = "library.getartists";
 var formatJSON = true;
-var user = prompt("Input your user name, get top 200 artists")
+var user = prompt("Input your user name, get top 20 artists")
 
 var requestUrl = LASTFM_URL + "?" + "method=" + method + "&api_key=" +
 	LASTFM_KEY +
-	"&user=" + user + (formatJSON ? "&format=json" : "") + "&limit=200";
+	"&user=" + user + (formatJSON ? "&format=json" : "") + "&limit=20";
 
 var request = new XMLHttpRequest();
 request.open("GET", requestUrl);
@@ -17,9 +17,20 @@ request.onreadystatechange = function() {
 		console.log(responseData);
 		var text = "";
 		responseData.artists.artist.forEach(function(el, i) {
-			text += el.name + ", ";
+			var req2 = new XMLHttpRequest();
+			req2.open("GET", "http://musicbrainz.org/ws/2/artist/" + el.mbid +
+				"?fmt=json&inc=aliases");
+			req2.onreadystatechange = function() {
+				if (req2.readyState === 4) {
+					console.log("Got country for " + el.name)
+					var mbzdata = JSON.parse(req2.response);
+					text += el.name + " (" + mbzdata.area.name + "), ";
+					document.body.innerHTML = text;
+				}
+			}
+			req2.send();
+
 		})
-		document.body.innerHTML = text;
 	}
 };
 
