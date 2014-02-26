d468b3aafb1f96e35f09807905468a0d920c3c50
Daniel Molin <dmol@kth.se>
2014-02-26 16:25:59 +0100

    Geolocation from Google! There's a limit on number of queries though, 2400/24h. :thumbsdown: Maybe there's a better api or maybe it will have to do.

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+var geocoder = new google.maps.Geocoder();
+
+// This is run when we've got artists from last.fm
 var cb = function(error, responseData) {
 	console.log(responseData);
 	var text = "";
 
+    // Get country for each artist
 	responseData.artists.artist.forEach(function(el, i) {
+        // Get coutry from musicbrainz
         api.musicbrainz.lookup("artist", el.mbid, function(err, responseData2){
-            console.log("Got country for " + responseData2.name);
-		    text += el.name + " (" + responseData2.area.name + ", " + el.playcount +
-			    " plays), ";
-		    document.body.innerHTML = text;
+            // Return if something failed
+            if(err || !responseData2.area){
+                console.error("Couldn't get country from Musicbrainz", err, responseData2);                
+                return;
+            }
+            
+            // Get coordinates from googlgle
+            geocoder.geocode({"address":responseData2.area.name}, function(geoResults, status){
+                 // Add all info to a string
+		        text += el.name + " (" + responseData2.area.name + ", " + el.playcount+" plays)";
+
+                // Return on error
+                if(geoResults){
+                    text +=" <a href=http://www.openstreetmap.org/#map=7/"+
+                    geoResults[0].geometry.location.d+"/"+geoResults[0].geometry.location.e+
+                    " target='_blank'>map</a>";
+                }else{
+                    console.error("Couldn't get coordinates for "+el.name+", "+responseData2.area.name+" / Status: "+status);
+                } 
+                
+                // Show on page
+                text+=", ";
+		        document.body.innerHTML = text;
+            });   
         });
 	})
 }
