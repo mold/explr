9bd050e713f0a11fc12e4d1faa6a37926c1c2f2b
Daniel Molin <dmol@kth.se>
2014-02-28 12:28:26 +0100

    api.getTags works with localstorage And is a good example of how to use localstorage for caching! :))) :neckbearck:

--- static/script.js
+++ static/script.js
@@ -3,16 +3,16 @@ var country;
 
 // This is run when we've got artists from last.fm
 var cb = function(error, responseData) {
-	console.log(responseData);
-	var text = "";
+    console.log(responseData);
+    var text = "";
 
-	// Get country for each artist
-	responseData.artists.artist.forEach(function(el, i) {
-		// Get country from lastfm
-		api.getCountry(el.name, function(data) {
+    // Get country for each artist
+    responseData.artists.artist.forEach(function(el, i) {
+        // Get country from lastfm
+        api.getCountry(el.name, function(data) {
 
-		});
-	})
+        });
+    })
 }
 
 var
