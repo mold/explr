9d09658474a400f6325a90719c71b7972d0931a0
fymmot <tommyfeldt@gmail.com>
2014-02-28 11:32:14 +0100

    Country from lastfm tag

--- static/api/api.js
+++ static/api/api.js
@@ -4,55 +4,37 @@ if (!window.localStorage.artists) {
 	window.localStorage.artists = {};
 }
 
-d3.csv("../countries.csv",function(err, data){
+d3.csv("../static/countries.csv", function(err, data) {
+	data = d3.nest()
+		.key(function(d) {
+			return d.tag.toLowerCase();
+		})
+		.map(data);
 	api.getCountry = function(artist, callback) {
 		var ls = window.localStorage;
 		if (ls.artists[artist]) {
 			return ls.artists[artist].country_code;
 		} else {
+
 			// Get artists country code here, from last.fm or whatever
-			data = d3.nest()
-				.key(function(d){
-					return d.tag.toLowerCase();
+			console.log(data);
+
+			api.lastfm.send("artist.gettoptags", [["artist", artist]], function(err,
+				responseData2) {
+				// Return if something failed
+				if (err || !responseData2.toptags.tag) {
+					console.error("Couldn't get top tags from lastfm", err, responseData2);
+					return;
+				}
+				console.log(responseData2)
+				responseData2.toptags.tag.forEach(function(t, i) {
+					if (data[t.name.toLowerCase()]) {
+						console.log("Match! ", t.name, data[t.name][0].id)
+					}
 				})
-				.entries(data);
-
-				console.log(data);
-
-			api.lastfm.send("artist.gettoptags", [["artist", el.name]] function(err, responseData2){
-            // Return if something failed
-            if(err || !responseData2.toptags.tag){
-                console.error("Couldn't get top tags from lastfm", err, responseData2);                
-                return;
-            }
-            responsedata2.toptags.tag.forEach(function(t, i){
-                if (t.name == "swedish")
-                    
-            })
-            /*
-
-            // Get coordinates from googlgle
-            geocoder.geocode({"address":responseData2.area.name}, function(geoResults, status){
-                 // Add all info to a string
-		        text += el.name + " (" + responseData2.area.name + ", " + el.playcount+" plays)";
-
-                // Return on error
-                if(geoResults){
-                    text +=" <a href=http://www.openstreetmap.org/#map=7/"+
-                    geoResults[0].geometry.location.d+"/"+geoResults[0].geometry.location.e+
-                    " target='_blank'>map</a>";
-                }else{
-                    console.error("Couldn't get coordinates for "+el.name+", "+responseData2.area.name+" / Status: "+status);
-                } 
-                
-                // Show on page
-                text+=", ";
-		        document.body.innerHTML = text;
-                */
-            });   
-        });
+			});
 		}
-	}	
+	}
 })
 
 
--- static/script.js
+++ static/script.js
@@ -3,17 +3,19 @@ var country;
 
 // This is run when we've got artists from last.fm
 var cb = function(error, responseData) {
-    console.log(responseData);
-    var text = "";
+	console.log(responseData);
+	var text = "";
 
-    // Get country for each artist
-    responseData.artists.artist.forEach(function(el, i) {
-        // Get coutry from musicbrainz
-        api.getCountry(el.name,function(data){
-            console.log(data)
-        });
-    })
+	// Get country for each artist
+	responseData.artists.artist.forEach(function(el, i) {
+		// Get country from lastfm
+		api.getCountry(el.name, function(data) {
+
+		});
+	})
 }
+
+var
 
 // user = prompt("Input your user name, get top 20 artists")
 user = SESSION.name;
