6d241659f8f8df2b1b2b3a722719dbc874e7e17a
Mike Manger <mike.manger@lighthouseuk.net>
2019-07-05 15:50:27 +0100

    Use double quotes in gulp file.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,21 +6,21 @@
  * Gulp plugins
  */
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var del         = require('del');
-var sass        = require('gulp-sass');
-var sourcemaps  = require('gulp-sourcemaps');
-var prefix      = require('gulp-autoprefixer');
-var concat      = require('gulp-concat');
-var rename      = require('gulp-rename');
-var uglify      = require('gulp-uglify');
-var deporder    = require('gulp-deporder');
-var changed     = require('gulp-changed');
-var imagemin = require('gulp-imagemin');
-var minifyCss = require('gulp-minify-css');
-var ghPages = require('gulp-gh-pages');
-var minifyHTML  = require('gulp-minify-html');
+var gulp        = require("gulp");
+var browserSync = require("browser-sync").create();
+var del         = require("del");
+var sass        = require("gulp-sass");
+var sourcemaps  = require("gulp-sourcemaps");
+var prefix      = require("gulp-autoprefixer");
+var concat      = require("gulp-concat");
+var rename      = require("gulp-rename");
+var uglify      = require("gulp-uglify");
+var deporder    = require("gulp-deporder");
+var changed     = require("gulp-changed");
+var imagemin    = require("gulp-imagemin");
+var minifyCss   = require("gulp-minify-css");
+var ghPages     = require("gulp-gh-pages");
+var minifyHTML  = require("gulp-minify-html");
 
 
 
@@ -29,18 +29,18 @@ var minifyHTML  = require('gulp-minify-html');
  */
 var path = {
             build:{
-                css: 'build/assets/css/',
-                js: 'build/assets/js/',
-                img: 'build/assets/img/',
-                data: 'build/assets/data/',
-                html: 'build/'
+                css: "build/assets/css/",
+                js: "build/assets/js/",
+                img: "build/assets/img/",
+                data: "build/assets/data/",
+                html: "build/"
             },
             src:{
-                sass: 'src/assets/scss/',
-                js: 'src/assets/js/',
-                img: 'src/assets/img/',
-                data: 'src/assets/data/',
-                html: 'src/'
+                sass: "src/assets/scss/",
+                js: "src/assets/js/",
+                img: "src/assets/img/",
+                data: "src/assets/data/",
+                html: "src/"
             }
 };
 
@@ -52,17 +52,17 @@ var path = {
  * Pre-cleaning the build folder
  */
 
-gulp.task('clean', function() {
+gulp.task("clean", function() {
     return del([
-        'build/',
+        "build/",
     ]);
 });
 
 /**
  * Deploy to gh-pages branch! Run using 'gulp deploy'
  */
-gulp.task('upload', function(cb) {
-    gulp.src('./build/**/*')
+gulp.task("upload", function(cb) {
+    gulp.src("./build/**/*")
         .pipe(ghPages());
     cb();
 });
@@ -74,14 +74,14 @@ gulp.task('upload', function(cb) {
 /**
  * Compiles SCSS sourcefiles and outputs autoprefixed, minified CSS + sourcemaps
  */
-gulp.task('sass', function(cb) {
+gulp.task("sass", function(cb) {
     gulp.src(path.src.sass + "/*.scss")
     
         .pipe(sourcemaps.init())
-            .pipe(sass.sync().on('error', sass.logError)) //Log SCSS errors in console!
-            .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-            .pipe(rename('main.min.css'))
-            .pipe(minifyCss({compatibility: 'ie8'}))
+            .pipe(sass.sync().on("error", sass.logError)) //Log SCSS errors in console!
+            .pipe(prefix(["last 15 versions", "> 1%", "ie 8", "ie 7"], { cascade: true }))
+            .pipe(rename("main.min.css"))
+            .pipe(minifyCss({compatibility: "ie8"}))
 
         .pipe(sourcemaps.write("sourcemaps"))
         .pipe(gulp.dest(path.build.css))
@@ -93,12 +93,12 @@ gulp.task('sass', function(cb) {
 /**
  * Combines and minifies all source JavaScript files, including sourcemaps. Dependencies and ordering is done with the deporder plugin
  */
-gulp.task('js', function(cb){
-    gulp.src(path.src.js + '**/*.js')
+gulp.task("js", function(cb){
+    gulp.src(path.src.js + "**/*.js")
         .pipe(deporder())
         .pipe(sourcemaps.init())
-            .pipe(concat('concat.js'))
-            .pipe(rename('all.min.js'))
+            .pipe(concat("concat.js"))
+            .pipe(rename("all.min.js"))
             .pipe(uglify())
         .pipe(sourcemaps.write("sourcemaps"))
         .pipe(gulp.dest(path.build.js))
@@ -109,8 +109,8 @@ gulp.task('js', function(cb){
 /**
  * Optimizes images for web and outputs them to build folder.
  */
-gulp.task('img', function(cb) {
-    gulp.src(path.src.img + '*.*')
+gulp.task("img", function(cb) {
+    gulp.src(path.src.img + "*.*")
         .pipe(changed(path.build.img)) // Ignore unchanged files
         .pipe(imagemin({optimizationLevel: 5}))
         .pipe(gulp.dest(path.build.img))
@@ -122,11 +122,11 @@ gulp.task('img', function(cb) {
 /**
  * Outputs data files to build folder
  */
-gulp.task('data', function(cb) {
-    gulp.src(path.src.data + '*.*')
+gulp.task("data", function(cb) {
+    gulp.src(path.src.data + "*.*")
         .pipe(changed(path.build.data)) // Ignore unchanged files
         .pipe(gulp.dest(path.build.data));
-    gulp.src(path.src.data + '*.*')
+    gulp.src(path.src.data + "*.*")
         .pipe(browserSync.stream());
     cb();
 });
@@ -134,8 +134,8 @@ gulp.task('data', function(cb) {
 /**
  * Outputs html files to build folder
  */
-gulp.task('html', function(cb) {
-    gulp.src([path.src.html + '*.html', path.src.html + 'CNAME'])
+gulp.task("html", function(cb) {
+    gulp.src([path.src.html + "*.html", path.src.html + "CNAME"])
         .pipe(changed(path.build.html)) // Ignore unchanged files
         //.pipe(minifyHTML())
         .pipe(gulp.dest(path.build.html))
@@ -146,7 +146,7 @@ gulp.task('html', function(cb) {
 /**
  * Outputs all files.
  */
-gulp.task('build', gulp.parallel('sass', 'js', 'img', 'html', 'data'));
+gulp.task("build", gulp.parallel("sass", "js", "img", "html", "data"));
 
 // -----------------------------------------------------------------------------
 // Watch and serve tasks
@@ -155,7 +155,7 @@ gulp.task('build', gulp.parallel('sass', 'js', 'img', 'html', 'data'));
 /**
  * Start BrowserSync server and watch files for changes!
  */
-gulp.task('serve', function(cb) {
+gulp.task("serve", function(cb) {
 
     //Start browsersync server!
     browserSync.init({
@@ -164,9 +164,9 @@ gulp.task('serve', function(cb) {
         https: true,
     });
     //Watch folders!
-    gulp.watch(path.src.sass + "**/*.scss", gulp.series('sass'));
-    gulp.watch(path.src.html + '*.html', gulp.series('html'));
-    gulp.watch(path.src.js + "/**/*.js", gulp.series('js'));
+    gulp.watch(path.src.sass + "**/*.scss", gulp.series("sass"));
+    gulp.watch(path.src.html + "*.html", gulp.series("html"));
+    gulp.watch(path.src.js + "/**/*.js", gulp.series("js"));
     cb();
 });
 
@@ -177,9 +177,9 @@ gulp.task('serve', function(cb) {
 /**
  * Run tasks in specified order! (1. clean, 2. build, 3. serve and watch)
  */
-gulp.task('default', gulp.series('clean', 'build', 'serve'));
+gulp.task("default", gulp.series("clean", "build", "serve"));
 
 /**
  * Alternative: Build, then deploy to gh-pages!
  */
-gulp.task('deploy', gulp.series('clean', 'build', 'upload'));
+gulp.task("deploy", gulp.series("clean", "build", "upload"));
