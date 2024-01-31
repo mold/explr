843b85277ccf7aac4735cb6c5dda5082d14bacdb
Tommy Feldt <tommyfeldt@gmail.com>
2024-01-31 17:03:00 +0100

    Rewrite dialog using dialog, add checkbox to artists and save to local storage

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -10,6 +10,7 @@ var script = script || {};
 let loadingReady = false;
 let loadingStatus = loadingReady ? "Ready to Explr!" : "Loading...";
 let announcementIntervalId;
+let noCountryArtistSortMethod = "scrobbles";
 
 
 var STORED_ARTISTS;
@@ -48,30 +49,114 @@ var countryCountObj = {};
     var randomcountrylist = ["Malawi", "Malaysia", "Peru", "Sierra Leone", "Trinidad & Tobago", "Greece", "Laos", "Iran", "Haiti", "Nicaragua", "Mongolia", "Slovakia"];
     var listOfArtistsWithNoCountry = [];
 
-    /**
-     * adds artists with no country to the array of artists with
-     * no country :)
-     * 
-     * @param {*} data Response from api.getCountries; array of
-     * artists that may or may not have country
-     */
+    function sortArtists(data, method) {
+        if (method === "scrobbles")
+            return data.sort((a, b) => b.playcount - a.playcount);
+        else if (method === "name")
+            return data.sort((a, b) => a.artist.localeCompare(b.artist));
+    }
+    
     var addArtistsWithNoCountry = function (data) {
         listOfArtistsWithNoCountry = listOfArtistsWithNoCountry.concat(data);
-
-        var noCountriesListEl = d3.select(".no-countries ul");
-        data.forEach(function (_art) {
-            noCountriesListEl.append("li").html('<a href="' + _art.url + '" target="blank" class="no-countries__link">' + _art.artist + '</a>');
-        })
-
-        d3.select(".no-countries__info").html(listOfArtistsWithNoCountry.length + " artists without a country:");
-
         saveToStorage("no_countries", listOfArtistsWithNoCountry);
+    
+        function handleCheckboxChange() {
+            let artistName = this.id;
+            let checked = this.checked;
+            let artistsState = JSON.parse(localStorage.getItem('noCountryArtistsProgress')) || {};
+            artistsState[artistName] = { artistName, checked };
+            localStorage.setItem('noCountryArtistsProgress', JSON.stringify(artistsState));
+            // get the label element for the filter checked checkbox
+            let filterCheckedLabel = document.querySelector("label[for='hide-checked']");
+            // Update the label to include the number of checked artists
+            filterCheckedLabel.innerHTML = `Hide checked artists (${document.querySelectorAll("dialog[open] ul li input[type='checkbox']:checked").length})`;
+            
+        }
 
-        if (listOfArtistsWithNoCountry.length) {
-            d3.select(".no-countries").style({
-                visibility: "visible",
-                "pointer-events": "all",
+        
+    
+        function updateNoCountriesList() {
+            let artistsState = JSON.parse(localStorage.getItem('noCountryArtistsProgress')) || {};
+            const sortedData = sortArtists(listOfArtistsWithNoCountry, noCountryArtistSortMethod);
+            var noCountriesListEl = d3.select(".no-countries__content ul");
+            noCountriesListEl.html("");
+            sortedData.forEach(function (_art) {
+                let artistState = artistsState[_art.artist] || { artistName: _art.artist, checked: false };
+                let listItem = noCountriesListEl.append("li");
+                listItem.append("input")
+                    .attr("type", "checkbox")
+                    .property("checked", artistState.checked)
+                    .attr("id", _art.artist)
+                    .on("change", handleCheckboxChange);
+                listItem.append("label")
+                    .attr("for", _art.artist)
+                    .html('<a href="' + _art.url + '" target="blank" class="no-countries__link">' + _art.artist + '</a><span>' + _art.playcount + ' scrobbles</span>');
+                if (document.querySelector("#hide-checked")?.checked && artistState.checked) {
+                    listItem.style("display", "none");
+                }
+            })
+            d3.select(".no-countries__info").html(listOfArtistsWithNoCountry.length + " artists without a country:");
+        }
+
+        // Add the checkbox next to the filter radios
+        d3.select("dialog fieldset").append("input")
+            .attr("type", "checkbox")
+            .attr("id", "hide-checked")
+            .on("change", updateNoCountriesList);
+        d3.select("dialog fieldset").append("label")
+            .attr("for", "hide-checked")
+            .text("Hide checked artists");
+    
+        // Handle sorting with radios
+        let radios = document.getElementsByName('sort');
+        function sortFunction() {
+            let selectedValue;
+            for (let radio of radios) {
+                if (radio.checked) {
+                    selectedValue = radio.value;
+                    noCountryArtistSortMethod = selectedValue;
+                    updateNoCountriesList();
+                    break;
+                }
+            }
+        }
+    
+        for (let radio of radios) {
+            radio.addEventListener('change', sortFunction);
+        }
+    
+        updateNoCountriesList("scrobbles");
+    
+        document.querySelector(".no-countries__title").addEventListener("click", function () {
+            const dialog = document.querySelector(".no-countries__content");
+            dialog.setAttribute("open", "open");
+    
+            document.querySelector("#no-countries__heading").focus();
+
+            // Update the label to include the number of checked artists
+            let filterCheckedLabel = document.querySelector("label[for='hide-checked']");
+            console.log(document.querySelectorAll("dialog[open] ul li input[type='checkbox']:checked").length)
+            filterCheckedLabel.innerHTML = `Hide checked artists (${document.querySelectorAll("dialog[open] ul li input[type='checkbox']:checked").length})`;
+    
+            document.addEventListener("keydown", function (e) {
+                if (e.keyCode == 27) {
+                    const dialog = document.querySelector(".no-countries__content");
+                    dialog.removeAttribute("open");
+                    document.querySelector(".no-countries__title").focus();
+                }
             });
+        });
+    
+        document.querySelector(".no-countries__content .close").addEventListener("click", function () {
+            const dialog = document.querySelector(".no-countries__content");
+            dialog.removeAttribute("open");
+            document.querySelector(".no-countries__title").focus();
+        });
+    
+        if (listOfArtistsWithNoCountry.length) {
+            setTimeout(function () {
+                document.querySelector(".no-countries").classList.remove("hidden");
+            }, 850);
         }
     }
 
